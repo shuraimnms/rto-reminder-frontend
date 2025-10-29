5bf5050c2b6b45f922d140ef7d0536f32637a9a9
Shuraim <shuraimnms@gmail.com>
2025-10-29 16:48:50 +0530

    Updated scheduler logic

--- src/utils/razorpay.js
+++
@@ -1,3 +0,0 @@
-// Razorpay utility for frontend integration
-// Note: This is a placeholder for Razorpay checkout integration
-// The actual Razorpay instance will be created in the component when needed
